fad8e51dde51cb494df16dd86e67ecc5c469fe44
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2022-07-30 20:08:52 +0530

    Update 101_Genomewide_with_best_K.sh

--- Supplementary_figure_Admixture/101_Genomewide_with_best_K/101_Genomewide_with_best_K.sh
+++ Supplementary_figure_Admixture/101_Genomewide_with_best_K/101_Genomewide_with_best_K.sh
@@ -1,10 +1,7 @@
+## For the genomewide Admixture analysis of 101 individuals use genotype likelihood approach and genarate beagle.gz format using ANGSD
 angsd -GL 2 -out genolike_101 -nThreads 32 -minMapQ 30 -minQ 20 -doGlf 2 -doMajorMinor 1 -SNP_pval 1e-6 -doMaf 1 -bam 101.PCA_bamlist
 
-
-
-
-
-
+##using beagle.gz format as input in NGSadmix get estimated covariance matrix in .cov output file
 for j in {1..15}
 do
 for i in {1..15}
