d0df6a35bf205cf8976c08940b4300afe095aaef
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2022-07-30 20:07:45 +0530

    Update Admixture_101_without_Z_scaff_MT_W.sh

--- Supplementary_figure_Admixture/101_individual_exclude_Z_W_scaffold_MT/Admixture_101_without_Z_scaff_MT_W.sh
+++ Supplementary_figure_Admixture/101_individual_exclude_Z_W_scaffold_MT/Admixture_101_without_Z_scaff_MT_W.sh
@@ -1,9 +1,11 @@
+
+#Run angsd only for autosomes using genotype likelihood approach and genarate beagle.gz format
 angsd -GL 2 -out genolike_allblack101_all_autosomes -minMapQ 30 -minQ 20 -doGlf 2 -doMajorMinor 1 -SNP_pval 1e-6 -doMaf 1 -bam 101.PCA_bamlist -rf autosomes.bed
 
 
 
 
-
+##using beagle.gz format as input in NGSadmix get estimated covariance matrix in .cov output file
 for i in 2 3 4 5 6 7 8 9 10
 do
 NGSadmix -likes genolike_allblack101_all_autosomes.beagle.gz -K $i -P 32 -o $i.admixture -minMaf 0.05 -minInd 70
