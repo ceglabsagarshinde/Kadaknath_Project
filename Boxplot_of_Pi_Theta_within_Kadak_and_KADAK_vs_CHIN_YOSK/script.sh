8b32de9fd6542dbab51e97db3f9883e50d1d2d5b
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2022-08-01 01:10:05 +0530

    Update script.sh

--- Boxplot_of_Pi_Theta_within_Kadak_and_KADAK_vs_CHIN_YOSK/script.sh
+++ Boxplot_of_Pi_Theta_within_Kadak_and_KADAK_vs_CHIN_YOSK/script.sh
@@ -1,11 +1,14 @@
+#extract the columns within the population statistics file of ANGSD for each windows of start and end, Pi, theta with Nsites.
 sed 's/(/\t(/3' Korean.thetasWindow.gz.pestPG |cut -f2,3,5,6,15 |sed 's/^/Korean_black\t/g' > Korean.boxplot
 sed 's/(/\t(/3' CHINA_9IND.thetasWindow.gz.pestPG |cut -f2,3,5,6,15 |sed 's/^/Chinese_black\t/g' > Chinese.boxplot
 sed 's/(/\t(/3' KADAKNATH_9IND.thetasWindow.gz.pestPG |cut -f2,3,5,6,15 |sed 's/^/India_black\t/g' > India.boxplot
 
+
+#merge file and remove header
 cat India.boxplot Chinese.boxplot Korean.boxplot  |grep -v "Chr" >all_boxplot
+
+#replace the names
 sed -i  's/India_black/KADK/g' all_boxplot
-
-
 sed -i  's/Chinese_black/CHIN/g' all_boxplot
 sed -i  's/Korean_black/YOSK/g' all_boxplot
 
