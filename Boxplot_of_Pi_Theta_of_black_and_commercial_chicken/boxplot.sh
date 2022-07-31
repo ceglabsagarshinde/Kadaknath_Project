5ec0c0afdbeb0a6dd47189a304e23328ca315871
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2022-08-01 01:02:12 +0530

    Update boxplot.sh

--- Boxplot_of_Pi_Theta_of_black_and_commercial_chicken/boxplot.sh
+++ Boxplot_of_Pi_Theta_of_black_and_commercial_chicken/boxplot.sh
@@ -1,5 +1,7 @@
+#extract the columns from within pop statastics file of ANGSD for Pi, theta, Tajima's D and Fu ald Li's D with Nsites
 for i in *gz.pestPG;do cut -f4,5,9,11,14 $i >$i.subset;done
 
+#population-wise add the short names
 sed -i 's/^/BROC7\t/g' BROC_7.thetasWindow.gz.pestPG.subset
 sed -i 's/^/CHIN9\t/g' CHINA_9IND.thetasWindow.gz.pestPG.subset
 sed -i 's/^/CHIN17\t/g' china.thetasWindow.gz.pestPG.subset
@@ -22,9 +24,9 @@ sed -i 's/^/YOSK3\t/g' YOSK.thetasWindow.gz.pestPG.subset
 sed -i 's/^/GGMI3\t/g' GGMI.thetasWindow.gz.pestPG.subset
 
 
-
+#merge all files and make single file
 cat jetblack.thetasWindow.gz.pestPG.subset Golden.thetasWindow.gz.pestPG.subset pencill.thetasWindow.gz.pestPG.subset KADAKNATH_9IND.thetasWindow.gz.pestPG.subset KADAKNATH.thetasWindow.gz.pestPG.subset  LCEM.thetasWindow.gz.pestPG.subset LCMY.thetasWindow.gz.pestPG.subset LCTMJ.thetasWindow.gz.pestPG.subset  XBBC.thetasWindow.gz.pestPG.subset TBTC.thetasWindow.gz.pestPG.subset CHINA_9IND.thetasWindow.gz.pestPG.subset china.thetasWindow.gz.pestPG.subset YOSK.thetasWindow.gz.pestPG.subset Korean.thetasWindow.gz.pestPG.subset BROC_7.thetasWindow.gz.pestPG.subset CWHLL.thetasWindow.gz.pestPG.subset CWLH_6.thetasWindow.gz.pestPG.subset WHLL.50thetasWindow.gz.pestPG.subset RIRL_9.thetasWindow.gz.pestPG.subset  GGMI.thetasWindow.gz.pestPG.subset >boxplot
-
+##remove the header
 grep -v "nSites" boxplot >all.boxplot
 
 
