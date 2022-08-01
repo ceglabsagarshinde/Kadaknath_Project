50906b39835b0e4ce34d5c7e1443b2164772d98a
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2022-08-01 12:12:39 +0530

    Delete Boxplot_of_Pi_Theta_within_Kadak_and_KADAK_vs_CHIN_YOSK/WITHIN_KADK_BOX_PLOT directory

--- Boxplot_of_Pi_Theta_within_Kadak_and_KADAK_vs_CHIN_YOSK/WITHIN_KADK_BOX_PLOT/script.sh
+++
@@ -1,6 +0,0 @@
-sed 's/(/\t(/3' Golden.thetasWindow.gz.pestPG |cut -f2,3,5,6,15 |sed 's/^/GOLD\t/g' > GOLD.boxplot
-sed 's/(/\t(/3' jetblack.thetasWindow.gz.pestPG |cut -f2,3,5,6,15 |sed 's/^/JETB\t/g' > JETB.boxplot
-sed 's/(/\t(/3' pencill.thetasWindow.gz.pestPG |cut -f2,3,5,6,15 |sed 's/^/PENC\t/g' > PENC.boxplot
-
-cat GOLD.boxplot JETB.boxplot PENC.boxplot |grep -v "Chr" >all_boxplot
-
