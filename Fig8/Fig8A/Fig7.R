625b89cd560dbd21294c2552989221c085428543
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2023-06-01 18:19:27 +0530

    Add files via upload

---
+++ Fig8/Fig8A/Fig7.R
@@ -0,0 +1,17 @@
+library(qqman)
+
+kadk_china<-read.table("kadk_9ind.china_9ind.50kb.column.txt",header=T)
+tiff(file="Fig7A.tif",res=30,width=5000,height=3700)
+#par(mai=c(6,11,5,3), mgp=c(40,15,3))
+par(mfrow=c(3,1))
+par(mai=c(6,10,2,2), mgp=c(40,15,3))
+manhattan(kadk_china, chr="chr", bp="BP", snp="SNP", p="P" ,ylim=c(0,1),logp=F, chrlabs=c(1:28,30,31,32,33,c(rep("scaffold",14)),"Z"),col=c("darkslategray3","deepskyblue"),ylab=expression(paste("F"[ST])),cex=15,cex.lab=20,cex.axis=17,xlab="Chromosomes",font.axis=2,yaxt='n')
+axis(2,at=c(0.2,0.4,0.6,0.8), lwd = 20,tck = -0.02,cex.axis=15,font = 2,las=2)
+#abline(h=c,col="black",lty=2,lwd=20)
+box(lwd=30)
+#text(mean(A$BP),0.9,"KADK vs. Chinese",cex=25,font=2)
+#mtext(side=3, "kadk vs. chinese",font=2,cex=17,outer = F,line= +5)
+rect(0, 0.363558, 960000000, 0.363558, cex=100, col="black",lty=2,lwd=10)
+rect(960000000,0.784523, 1045000000, 0.784523, cex=100, col="black",lty=2,lwd=10)
+dev.off()
+
---
+++ Fig8/Readme.md
@@ -0,0 +1,8 @@
+Fig7: This folder contains the following subfolders:
+
+1. Fig7A: This folder contains the input files, code, and figure for the pairwise Fst comparison between Kadaknath (KADK) and Chinese black-bone chicken (CHIN).
+
+2. Fig7B: This folder contains the input files, code, and figure for the pairwise Fst comparison between Kadaknath (KADK) and Chinese black bone chicken  (CHIN) in 10Kb, Pi, and iHS of chromosome 20.
+
+
+
