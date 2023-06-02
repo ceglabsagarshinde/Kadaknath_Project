b3453a60573bd4719044da705d65a1be223802fe
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2023-06-02 15:00:05 +0530

    Delete iHS_XP-EHH_Fst_comparision_Dxy_main_fig3_AB_supplementary directory

--- iHS_XP-EHH_Fst_comparision_Dxy_main_fig3_AB_supplementary/dxy_vs_Fst_between_CHIN_KADK/dxy_vs_fst.sh
+++
@@ -1,8 +0,0 @@
-#convert vcf to geno format
-python parseVCF.py -i commons_snps_bcftools.vcf.recode.vcf --skipIndels| bgzip > commons_bcftools_SNPs_only.vcf.gz.geno.gz
-
-
-#calculate Dxy and Fst of autosomes
-popgenWindows.py -g commons_bcftools_SNPs_only.vcf.gz.geno.gz -o subset.Fst.Dxy.pi.csv.gz -f phased -w 50000 -s 50000 -p KADK -p CHIN --popsFile pop.info
-
-sed 's/,/\t/g' subset.Fst.Dxy.pi.csv > dxy.txt
